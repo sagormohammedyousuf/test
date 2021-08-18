003ba0169eff025b7789ba49472a72fd2684779a
Yousuf <smyousuf7999@gmail.com>
2021-08-19 00:29:38 +0600

    first commit

---
+++ dist/index.dev.js
@@ -0,0 +1,635 @@
+"use strict";
+
+// typeof("sagor ali");
+
+/*
+jaflk
+alfjlk*/
+// onload="alert(' please wait');"
+//  var name = "sagor" 
+//  var age = 21
+// //  document.write(name + age);
+// var name ,age ;
+// name = "murshida";
+// age = 19;
+// // document.write( name + age );
+// age = toString(num)
+// console.log(typeof(num));
+// var number = "20.5426";
+// num = toString(num)
+// num = parseFloat (num)
+// document.write(typeof(num));  
+// document.write(number.toFixed(3)) 
+// document.write(number.toPrecision(1));
+// document.write(typeof(Number("23")))
+// document.write(Number("23"))
+// var firstName = "Sagor Mohammed"
+// var lastName = "Yousuf "
+// var fullName = firstName + lastName;
+// document.write("My name is "  +  fullName);
+// var num1 = 34;
+// var num2 = 45;
+// document.write("nu1 = " + num1 +"num2 = "+ num2);
+// var text = "bangladesh vs pakistan";
+// var len = text.length;
+// document.write("number of character" + text.length);
+
+/* var text = prompt("Please enter your name : ");
+
+// var len = text.length;
+document.write("My Name is" + text); */
+// var base = prompt('Enter Base = ');
+// var height = prompt ("Enter Base = ") 
+// var area = base * height;
+// document.write("area = " + area);
+// var base = parseFloat(prompt('Enter Base = '));
+// var height = parseFloat(prompt ("Enter hieght = "));
+// var area = base * height;
+// document.write("area = " + area);
+// var num1 = "20";
+// var num2 =20 ;
+// document.write(num1===num2);
+// var num = 34 ;
+// if(num%2==0)
+// document.write("Even");
+// else 
+// document.write("odd")
+// var mark = prompt("Enter Your mark:");
+// if (mark < 33 )
+// document.write("fail"); 
+// else if (mark >  33)
+// document.write("pass");
+// else if (mark < 50 )
+// document.write("a-");
+// else("a+")
+// mark check if else pracice
+
+/*
+var marks = prompt("Enter Your marks : ");
+
+if (marks >=  80)
+document.write("A+");
+
+else if (marks >= 70)
+document.write("A");
+
+else if (marks >= 60)
+document.write("A-");
+
+else if (marks >= 50)
+document.write("B");
+
+else if (marks >= 40)
+document.write("C");
+
+else if  (marks >= 33)
+document.write("D");
+
+else
+document.write("Fail");
+*/
+// large number check
+
+/*
+           
+var num1 = prompt("enter num1 : ");
+var num2 = prompt("enter num2 : ");
+var num3 = prompt("enter num3 : ");
+
+if (num1 > num2 && num1 > num3)
+    document.write("large Number =" + num1);
+
+    else if (num2 > num1 && num2 > num3)
+    document.write("large Number =" + num2);
+
+else 
+document.write("Large number = " + num3);
+*/
+// vowel consonat check //
+
+/*
+
+var letter = prompt("Ener a letter : ");
+
+if (letter=="a" || letter =="e" || letter=="i" || letter=="o" || letter=="u" )
+document.write("vowel");
+
+else
+document.write("consonat");
+*/
+
+/*
+
+var digit = prompt("Enter any digit : ");
+
+switch (digit) {
+    case "0":
+        document.write("zero");
+        break;
+
+    case "1" :
+        document.write("one");
+        break;
+        
+    case "2" :
+        document.write("tow");
+        break;
+
+        
+    case "3" :
+        document.write("three");
+        break;
+
+        
+    case "4" :
+        document.write("four");
+        break;
+
+        
+    case "5" :
+        document.write("five");
+        break;
+
+        
+    case "6" :
+        document.write("six");
+        break;
+
+        
+    case "7" :
+        document.write("seven");
+        break;
+
+        
+    case "8" :
+        document.write("eight");
+        break;
+
+        
+    case "9" :
+        document.write("nine");
+        break;
+
+        
+    case "10" :
+        document.write("ten");
+        break;
+
+        
+    case "11" :
+        document.write("eleven");
+        break;
+
+        
+    case "12" :
+        document.write("twelve3");
+        break;
+
+
+
+    default:
+        document.write("not a digit ");
+}
+
+
+*/
+
+/*
+
+
+var digit = prompt("Enter Your Roll  : ");
+
+switch (digit) {
+    
+
+    case "1" :
+        document.write("Yousuf");
+        break;
+        
+    case "2" :
+        document.write("sagor");
+        break;
+
+        
+    case "3" :
+        document.write("Murshida");
+        break;
+
+        
+    case "4" :
+        document.write("Mim");
+        break;
+
+        
+    case "5" :
+        document.write("Eva");
+        break;
+
+        
+    case "6" :
+        document.write("আতিফ");
+        break;
+
+    
+
+
+    default:
+        document.write("<h2>  Sorry invalid   </h2> ");
+}
+
+*/
+
+/*
+
+var letter = prompt("Enter any single letter : ");
+letter = letter.toLowerCase();
+
+switch(letter){
+    case "a":
+        document.write("vowel")
+        break
+
+        case "e":
+        document.write("vowel")
+        break
+
+        case "i":
+        document.write("vowel")
+        break
+
+        case "o":
+        document.write("vowel")
+        break
+
+        case "u":
+        document.write("vowel")
+        break
+
+       default:
+           document.write("consonat");
+        
+}
+
+*/
+
+/*
+
+var letter = prompt("Enter any single letter : ");
+letter = letter.toLowerCase();
+
+switch(letter){
+    case "a":
+    case "i":
+    case "e":
+    case "o":
+        
+        document.write("vowel");
+        break; 
+
+        default:
+            document.write("consonant");
+}
+*/
+
+/*
+
+var marks = prompt("Please Enter Any letter : ");
+marks = marks.toLowerCase();
+
+switch(marks){
+    case "a":
+    case "i" :
+    case "e" :
+    case "o" :
+    document.write("vowel");
+    break;
+
+    default:
+        document.write("consonat");
+}
+
+*/
+// লুপ for loop
+// for( var x = 1; x <= 10; x = x + 1 )
+// {
+//     document.write("pakistan");
+// }
+// document.write("end");
+// for (var x = 1; x <=10; x++1){
+//     document.write('' + x);
+// }
+// while loop
+// var i = 1;
+// while (i <= 10) {
+//     document.write(" " + i );
+//     i = i + 2 
+// }
+// var vaterDam = prompt("enter Your buget : ");
+// if (vaterDam < 30 ){
+//     document.write("bhat den  vat khabo ")
+// }
+// else{
+//     document.write("vat nei porota khan");
+// }
+// var age = prompt("Enter Your ege ");
+// if(age >= 18 ){
+//     document.write("vote dite parbe ");
+// }
+// else{
+//     document.write("E tui ekhane ki koris ja vag , 18 na hole vote dite parbi na bujhli  ")
+// }
+// var age = prompt("Enter Your Age : ");
+// switch(age){
+//     case "age >= 18" :
+//     document.write("ja vote di");
+//     break
+//     default:
+//         document.write("ja vag kiser vote re tor ")
+// }
+// var budget = prompt("Enter Yousr Budget : ");
+// if(budget >= 200){
+//     document.write("ha niye jan apple ")
+// }
+// else{
+//     document.write("na hobe na")
+// }
+// var letter = prompt("Enter Eny Letter : ");
+// letter = letter.toLowerCase();
+// switch(letter){
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//     document.write("Vowel");
+//     break
+//     // case "1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 0":
+//     //     document.write("number")
+//     //     break
+//     default:
+//         document.write("consonat")
+// }
+// var beton = prompt("Please offer Your tiution fee, how much you can pay  : ");
+// var isChatri = prompt(" meye student =  Yes or No ");
+// if(beton >= 4000 || isChatri == yes){
+//     document.write("Kopal khule geche ");
+// }
+// else{
+//     document.write("fokira marka tiutioni");
+// }
+// var friends = ["sagor" , "yousuf" , "murshida" , "mim" ];
+// document.write("yousuf");
+// var kotoBar = 0;
+// while(kotoBar < 10){
+//     document.write("ek bar kan dhore uthbos korchi ");
+//     kotoBar ++;
+// }
+// for (var x=1; x <= 10; x=x){
+//     document.write("<h1> Bangladesh </h1>");
+// }
+// document.write("end")
+// for (var x = 1; x x <= 10; x = x ++){
+//     document.write("<h1>Pakistan</h1>");
+// }
+// for ( var x = 1; x <= 20; x = x ++ ;){
+// }
+// document.write("end")
+// var x = 0 ;
+// while(x <=10){
+//     x ++
+//     document.write(" " + x);
+// }
+//  var x = 0 ;
+//  do{
+//      document.write(" " + x );
+//      x ++;
+//  }while (x <= 50);
+// for ( var x = 1; x <= 100; x++){
+//     if (x == 10 ){
+//         break;
+//     }
+//     document.write(" " + x );
+// }
+// document.write("<h1>I Love Murshida Mim  </h1>");
+// for ( var x = 1; x <= 100; x++){
+//     if (x == 10 ){
+//         continue;
+//     }
+//     document.write(" " + x  + " <br> ");
+// }
+// document.write("<h1>I Love Murshida Mim  </h1>");
+// function
+// function sagor(){
+//     var num = 5 ;
+//     var result = num * num ;
+//     document.write("Result = "+ result + "<br>");
+// }
+// sagor();
+// function sagor(num,num1,num2){
+//     var result = num * num1 * num2 ;
+//     document.write("Result = "+ result + "<br>");
+// }
+// sagor(2, 3,3);
+// function sagor(num, num1){
+//     var result = num * num1 ;
+//     // return result;
+// }
+// // var a = sagor(7,9);
+// // document.write(a )
+// sagor(10,90);
+// sagor(78,30);
+//  var num = ["0","1", "2", "3" , "4","5"]
+//  for( var i = 0; i <=  5; i++){
+//     document.write(num[i]);
+//  }
+// var x = 0 ;
+// while( x <= 100){
+//     x++
+//     document.write(" " + x);
+// }
+// var num =  ["0","1", "2", "3" , "4","5"];
+// num.shift();
+// console.log(num);
+// var num =  ["0","1", "2", "3" , "4","5"];
+// num.unshift("sagor");
+// console.log(num);
+// var n = ["0","1", "2", "3" , "4","5"];
+// document.write(n);
+// adding Element
+// n.splice(2,0,"karim","rahim");
+// removeEvent
+// n.splice(1,2);
+// document.write(n);
+// var nw = n.slice();
+// document.write(nw);
+// console.log(n)
+// var n = [ "sagor ", "yousuf", "anower" , " sharmin", "mama"]
+// var sortedn = n.sort();
+// console.log(sortedn); 
+// document.write(sagor); 
+// var nams = ["sagor" ,"mim" ,"Yousuf" ,"murshida"];
+// names.push("eva");
+// console.log(names);
+// function obj (nam , age , gpa,  lang){
+//     this.nam = nam ;
+//     this.age = age ;
+//     this.gpa = gpa ;
+//     this.lang = lang ;
+// }
+// var stu = new obj("sagor " , 20 , 3.93 , ["Bangla " , "hindi" , " English ", "urdu"]);
+// var st = new obj("yousuf" , 21 , 3.44 , ["Bangla " , "hindi" , " English ", "urdu"]);
+// console.log(st.age); 
+// function obj (nam , age , gpa,  lang){
+//     this.nam = nam ;
+//     this.age = age ;
+//     this.gpa = gpa ;
+//     this.lang = lang ;
+//     this.display = function(){
+//         console.log(this.nam);
+//         console.log(this.age);
+//         console.log(this.gpa);
+//         console.log(this.lang);
+//     }
+// }
+// var stu = new obj("sagor " , 20 , 3.93 , ["Bangla " , "hindi" , " English ", "urdu"]);
+// var st = new obj("yousuf" , 21 , 3.44 , ["Bangla " , "hindi" , " English ", "urdu"]);
+// st.display(nam);
+// console.log("ssgo");
+// document.getElementById("love").innerHTML = "hello kolija";
+// var change = document.getElementById("murshida")
+// .innerHTML = " ami murdhida ke biye korte chai ";
+// document.getElementsByTagName(" h1 ") [0].innerHTML = " Hi kolija ";
+// document.getElementsByClassName("head0")[0].innerHTML = " ami murshida ke biye korte chai  , amr tar jonno pagol hoye jabo ";
+// // var kolija = document.getElementsByClassName("head1")[1].innerHTML = " ami murdhida ke biye korbo ";
+//  document.querySelector(".head1").innerHTML = " ami murdhida ke biye korbo ";
+// function myImg(){
+//     document.write("who are You ?");
+// }
+// var myVar = document.querySelector("#myImg");
+// function myPic(){
+//     myVar.src ="img/y.png";
+// } 
+// function myKobita(){
+//     myVar.src ="img/kobita.png";
+// } 
+// var h1 = document.getElementsByTagName("h1")[0];
+// h1.innerHTML = " <h3> km farzana mim  apu ami apner dudu chuste chai</h3>"
+// var head1 = document.createElement("h3");
+// var text = document.createTextNode("oh km mim apu apner sathe ses korte chai");
+// head1.appendChild(text);
+// var maydiv =  document.getElementById("my-div");
+// maydiv.appendChild(head1);
+// var head2 = document.getElementsByTagName("h1")[1];
+// maydiv.removeChild(head2);
+// var img = ["img/y.png;", "img/kobita.png"]
+// var imgTag = document.querySelector("img");
+// var count = o;
+// function next(){
+//     count++;
+//     if(count >= img.length){
+//         count = 0;
+//         imgTag.src = img [count];
+//     } else{
+//         imgTag.src = img [count];
+//     }
+//     // imgTag.src=img[count];
+// }
+// function prev(){
+// }
+// var nam = "murshida";
+// function first (){
+//     var welcome = " hello";
+//     second ();
+//     document.write( welcome + " " + nam);
+// }
+// function second(){
+//     var welcome2 = " i love you"
+//     third();
+//     document.write(welcome2 + "  "+ nam + "<br>");
+// }
+// function third(){
+//     var welcome3 = " i want to marry you  ";
+//     document.write(welcome3 + nam + "<br>");
+// }
+// first();
+// var len = document.querySelectorAll(".myButton").length;
+// for(var i=0; i <len ; i++){
+//     document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+//         var text = this.innerHTML;
+//         document.querySelector("h1").innerHTML = text + " is clicked";
+//     });
+// }
+// for(var i = 0 ; i < 3; i++){
+//     ocument.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+//     var text = this.innerHTML;
+//     console.log(text);
+//         switch(text){
+//             case "a":
+//                 var audio = new Audio("audio/at.mp3");
+//                 audio.play();
+//                 break ;
+//                 case "b":
+//                 var audio = new Audio("audio/ar.mp3");
+//                 audio.play();
+//                 break ;
+//                 case "c":
+//                 var audio = new Audio("audio/am.mp3");
+//                 audio.play();
+//                 break ;
+//         }
+//     });
+// }
+// var myCustomObj ={
+//     nam: "yousuf",
+//     age: 20,
+//     job:"sutudent",
+//     msg: function(){
+//         document.write(this);
+//     }
+// }
+// myCustomObj.msg();
+// function myFunc(){
+//     console.log(this)
+// }
+// new myFunc()
+// var rect = {
+//     width: 100,
+//     height: 40,
+//     draw: function(){
+//         document.write('I am a rectangle ' + ' ')
+//         // document.write('my width is ' + this.width)
+//         // document.write('my height' + this.height)
+//         this.printProperties()
+//     },
+//     printProperties: function(){
+//         document.write('my width is '  + this.width + ' ')
+//         document.write('my height' + this.height + ' ')
+//     }
+// }
+// rect.draw()
+// function myFunc(){
+//     console.log(this)
+// }  
+// myFunc()
+// var createRect = function (width, height){
+//     return{
+//         width: width,
+//         height:height,
+//         draw: function(){
+//             console.log(' I am a rectangel')
+//             this.printProperties()
+//             console.loge(this)
+//         },
+//         printProperties: function (){
+//             console.log('my width is '  + this.width + ' ')
+//             console.log('my height' + this.height + ' ')
+//         }
+//     }
+// }
+// var rect1 = createRect(20,78 )
+var love = "  <h1> I love you</h1> ";
+var nam = " <h1>Murshida </h1>";
+
+for (x = 0; x <= 100; x++) {
+  document.write(x + love + " " + nam + " " + "<hr>");
+}
